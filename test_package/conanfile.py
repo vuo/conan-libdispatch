c68e1cf27af1170519d00e2d9321076aeebece3c
Steve Mokris <smokris@softpixel.com>
2017-12-17 22:30:07 -0500

    Replace conanfile_directory (deprecated in Conan 0.30.0) with source_folder.

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -4,13 +4,13 @@ class LibdispatchTestConan(ConanFile):
     generators = 'qbs'
 
     def build(self):
-        self.run('qbs -f "%s"' % self.conanfile_directory)
+        self.run('qbs -f "%s"' % self.source_folder)
 
     def imports(self):
         self.copy('*.so', src='lib', dst='bin')
 
     def test(self):
-        self.run('qbs run -f "%s"' % self.conanfile_directory)
+        self.run('qbs run')
 
         # Ensure we only link to system libraries.
         self.run('! (ldd bin/libdispatch.so | grep "/" | egrep -v "\s/lib64/")')
